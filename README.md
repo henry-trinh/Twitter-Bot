fe114c03e262bb666ce09cb491f5a682a9dfae0f
henry-trinh <84833630+henry-trinh@users.noreply.github.com>
2022-09-11 19:03:52 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+AI bot using Twitter API. Uses Flask and Pythonai bot with Twitter API
+
+![](https://user-images.githubusercontent.com/87113091/181373266-acfa4e7d-bf64-4708-967e-810f1cfb4332.gif)
+
+Click Show above and it will launch the app in a new browser tab. It starts by running the main.py program.
+
+← README.md
+That's this file!
+
+← twitter_scraper_fetcher.py
+A Python file that contains functions which scrape data from Twitter.
+
+← config.py
+This file sets some global variables (variables that are used by other parts of the program).
+
+← requirements.txt
+This file keeps track of which library versions we are using.
+
+← main.py
+This main Python script starts the webpage and processes the scraper requests.
+
+← static/ folder
+This folder contains the JavaScript, CSS and images that the web app uses.
+
+← homepage.html
+This launches the webpage of the Twitter app.
+
+Host Site: http://intro-to-python-ii.glitch.me/
---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+import json
+import config
+# from markov import generate_bot_answer
+from content_moderator import moderate
+from twitter_scraper_fetcher import *
+import random
+from markov import *
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# Renders UI
+@app.route("/")
+def home():
+  return render_template("homepage.html")
+
+# Chat API - WebSocket
+@socketio.on("send question")
+def generate_message(body, methods=["POST"]):
+  question = body["message"]
+  twitter_handle = body["username"]
+
+  # Call get_user_tweets() from twitter_scraper_fetcher.py to scrape some tweets
+  # tweets = get_user_tweets(twitter_handle)
+  
+  try:
+    bot_answer = generate_bot_answer(twitter_handle, question)
+    # cleaned_tweets = clean_tweets_data(tweets)
+    # Get a random tweet from the list of tweets
+   
+
+    # Send the answer to the app, to display to the user
+    answer = {"username": twitter_handle, "message": moderate(bot_answer)}
+    socketio.emit("bot answer", answer)
+  except:
+    bot_answer = "Sorry, I couldn't process that. Try again please."
+    socketio.emit("error", {"username": twitter_handle, "message": moderate(bot_answer)})
+
+if __name__ == "__main__":
+    socketio.run(app)
